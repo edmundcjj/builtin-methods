e322d481e8556669e5a2f2e2034e2aa6e3d7c064
Edmund <edmundcjj@gmail.com>
2018-05-08 10:38:02 +0800

    Edited to correct way of writing callback functions

--- index.js
+++ index.js
@@ -3,31 +3,31 @@
 function each(list, callback) {
     // Call the callback
     callback(list);
+}
 
-    function callback(list) {
-        // I'm the callback
-        for (var i = 0; i < list.length; i++) {
-          console.log(list[i]);
-        }
+function display(list) {
+    // I'm the callback
+    for (var i = 0; i < list.length; i++) {
+      console.log(list[i]);
     }
 }
 
 // Reduce Implementation
 function reduce(list, reducer, accumulator){
-  reducer(list, accumulator);
+  console.log("accumulator in reduce => ", accumulator);
+  return reducer(list, accumulator);;
+}
 
-  function reducer(list){
-    for (var i = 0; i < list.length; i++) {
-      accumulator += list[i];
-    }
-    return accumulator;
+function reducer(list, accumulator){
+  for (var i = 0; i < list.length; i++) {
+    accumulator += list[i];
   }
   return accumulator;
 }
 
 const list = [1,2,3];
-each(list, function() { });
+each(list, display);
 
 const number = [1,2,3];
-const sum = reduce(number, function() { }, 0);
+const sum = reduce(number, reducer, 0);
 console.log("Sum => ", sum);
