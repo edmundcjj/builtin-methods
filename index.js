9e92439b6f3ff16436461f165ed401ba412386d7
Edmund <edmundcjj@gmail.com>
2018-05-08 10:30:18 +0800

    Implemented reduce function

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 
+// Each Implementation with callbacks
 function each(list, callback) {
-    // ...
-
     // Call the callback
     callback(list);
-}
 
-function display(list) {
-    // I'm the callback
-    for (var i = 0; i < list.length; i++) {
-      console.log(list[i]);
+    function callback(list) {
+        // I'm the callback
+        for (var i = 0; i < list.length; i++) {
+          console.log(list[i]);
+        }
     }
 }
 
-let list = [1,2,3];
-each(list, display);
+// Reduce Implementation
+function reduce(list, reducer, accumulator){
+  reducer(list, accumulator);
+
+  function reducer(list){
+    for (var i = 0; i < list.length; i++) {
+      accumulator += list[i];
+    }
+    return accumulator;
+  }
+  return accumulator;
+}
+
+const list = [1,2,3];
+each(list, function() { });
+
+const number = [1,2,3];
+const sum = reduce(number, function() { }, 0);
+console.log("Sum => ", sum);
